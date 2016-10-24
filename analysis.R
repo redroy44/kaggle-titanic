d6cebb3185dd33b4cc6a79101fbb30f88f6e9f6e
Piotr Bandurski <redroy44@gmail.com>
2016-10-25 00:14:51 +0200

    added cool plot

--- analysis.R
+++ analysis.R
@@ -10,18 +10,23 @@ file_test <- "data/test.csv"
 # Load the train dataset
 df <- read.csv2(file_train, stringsAsFactors = F, header = T, sep = ",")
 
-# Load the data into dplyr's tbl_df
+# Load the data into dplyr's tbl_df and do some cleaning
 train_data <- tbl_df(df)
+train_data %>%
+  summarise_each(funs(sum(is.na(.)))) %>%
+  print
+
 train_data <- train_data %>%
-  mutate(Sex = as.factor(Sex), Embarked = as.factor(Embarked), Age = as.integer(Age), Fare = as.numeric(Fare)) %>%
+  mutate(Sex = as.factor(Sex), Embarked = as.factor(Embarked, exclude = NA), Age = as.integer(Age), Fare = as.numeric(Fare)) %>%
   separate(Name, into = c("Surname", "FirstName"), sep = ",") %>%
   separate(FirstName, into = c("Title", "FirstName"), sep = "\\.", extra = "merge") %>%
   mutate(Title = as.factor(Title))
 
 # Percentage of survivors per title
-test<-train_data %>%
+train_data %>%
   select(Title, Survived) %>%
   group_by(Title) %>%
-  summarize(count = n(), S = sum(Survived), percent = S/count) %>%
-  sum(count) %>%
-  print
+  summarize(count = n(), S = sum(Survived), P = S/count) %>%
+  arrange(desc(S)) %>%
+  ggplot(aes(x=Title, y=P)) + geom_bar(stat="Identity") +
+  labs(title = "Survival of Titles", x = "Title", y = "Percent Survived")
