b8fc1a6a6a5a868bcf9a6f985a86d88acc9f279a
Piotr Bandurski <redroy44@gmail.com>
2016-10-27 19:21:29 +0200

    simple glm

--- analysis.R
+++ analysis.R
@@ -38,7 +38,7 @@ train_data <- train_data %>%
   mutate(Sex = as.factor(Sex), Embarked = as.factor(Embarked), Age = as.integer(Age), Fare = as.numeric(Fare)) %>%
   separate(Name, into = c("Surname", "FirstName"), sep = ",") %>%
   separate(FirstName, into = c("Title", "FirstName"), sep = "\\.", extra = "merge") %>%
-  mutate(Title = as.factor(Title), Pclass = as.factor(Pclass), Survived = as.factor(Survived)) %>%
+  mutate(Title = as.factor(Title), Survived = as.factor(Survived), Pclass = as.factor(Pclass)) %>%
   select(-Surname, -FirstName, -Ticket, -Cabin)
 
 # Fix NA's in Age
@@ -50,13 +50,24 @@ train_data %>%
   summarise_each(funs(sum(is.na(.)))) %>%
   print
 
+# Prepare test set
+test_data <- test_data %>%
+  mutate(Sex = as.factor(Sex), Embarked = as.factor(Embarked), Age = as.integer(Age), Fare = as.numeric(Fare)) %>%
+  separate(Name, into = c("Surname", "FirstName"), sep = ",") %>%
+  separate(FirstName, into = c("Title", "FirstName"), sep = "\\.", extra = "merge") %>%
+  mutate(Title = as.factor(Title), Pclass = as.factor(Pclass)) %>%
+  select(-Surname, -FirstName, -Ticket, -Cabin)
+
+imputed_test <- complete(mice(test_data))
+test_data$Age <- imputed_test$Age
+test_data$Fare <- imputed_test$Fare
 # Time for some plots
 
 # Percentage of survivors per title
 p1 <- train_data %>%
   select(Title, Survived) %>%
   group_by(Title) %>%
-  summarize(count = n(), S = sum(Survived), P = S/count) %>%
+  summarize(count = n(), S = sum(as.integer(Survived)-1), P = S/count) %>%
   arrange(desc(S)) %>%
   ggplot(aes(x=Title, y=P)) + geom_bar(stat="Identity") +
   labs(title = "Survival of Titles", x = "Title", y = "Percent Survived")
@@ -75,7 +86,7 @@ plot(p2)
 p3 <- train_data %>%
   select(Sex, Survived) %>%
   group_by(Sex) %>%
-  summarize(count = n(), S = sum(Survived), P = S/count) %>%
+  summarize(count = n(), S = sum(as.integer(Survived)-1), P = S/count) %>%
   ggplot(aes(x=Sex, y=P)) + geom_bar(stat="Identity") +
   labs(title = "Survival per Sex", x = "Sex", y = "Percent Survived")
 plot(p3)
@@ -84,7 +95,7 @@ plot(p3)
 p4 <- train_data %>%
   select(Pclass, Survived) %>%
   group_by(Pclass) %>%
-  summarize(count = n(), S = sum(Survived), P = S/count) %>%
+  summarize(count = n(), S = sum(as.integer(Survived)-1), P = S/count) %>%
   ggplot(aes(x=Pclass, y=P)) + geom_bar(stat="Identity") +
   labs(title = "Survival per Pclass", x = "Pclass", y = "Percent Survived")
 plot(p4)
@@ -93,9 +104,20 @@ plot(p4)
 p5 <- train_data %>%
   select(Fare, Pclass) %>%
   group_by(Fare) %>%
-  #summarize(count = n(), S = sum(Survived), P = S/count) %>%
   ggplot(aes(x=Pclass, y=Fare)) + geom_boxplot() +
   labs(title = "Cost of Pclass", x = "Pclass", y = "Fare")
 plot(p5)
 
 grid.arrange(p4, p5, ncol=2)
+
+# Simple model
+
+glm_fit<-glm(Survived~.,data=select(train_data, -PassengerId, -Title, -Embarked),family=binomial(link='logit'))
+summary(glm_fit)
+
+# Write submission file
+Prediction <- predict(glm_fit, select(test_data, -PassengerId, -Title, -Parch, -Fare, -Embarked), type = "response")
+Survived <- round(Prediction)
+submit <- data.frame(PassengerId = test_data$PassengerId, Survived = Survived)
+write.csv(submit, file = "mysubmission.csv", row.names = FALSE)
+
